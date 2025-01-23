a8c5aceb68c0389e14f88d0ab268388edb7da408
Arturo Sánchez <danielarturosanchezgodoy@gmail.com>
2025-01-23 23:48:44 +0000

    End Crud MVC without persistence

--- src/main/java/com/cifpcm/Ejercicio_3_Animales/controllers/AnimalesController.java
+++ src/main/java/com/cifpcm/Ejercicio_3_Animales/controllers/AnimalesController.java
@@ -45,11 +45,22 @@ public class AnimalesController {
         return "animals/details";
     }
 
-    @PutMapping("/animales/editar/{id}")
+    @GetMapping("/animales/editar/{id}")
+    public String editarAnimal(@PathVariable int id, Model model){
+        String result = null;
+        Animal editAnimal = as.detail(id);
+        if(editAnimal != null){
+            model.addAttribute("animal", editAnimal);
+            result = "animals/edit";
+        }
+        return result;
+    }
+
+    @PostMapping("/animales/editar/{id}")
     public String editarAnimal(@PathVariable int id, @Valid @ModelAttribute Animal animal, BindingResult bindingResult){
         String result;
         if(bindingResult.hasErrors()){
-            result = "/animals/edit/"+animal.getId();
+            result = "animals/edit";
         } else {
             as.edit(id, animal);
             result = "redirect:/animales";
@@ -57,10 +68,23 @@ public class AnimalesController {
         return result;
     }
 
-
-
     @GetMapping("/animales/borrar/{id}")
     public String eliminarAnimal(@PathVariable int id, Model model){
-        return "animals/delete";
+        String result;
+        Animal deleteAnimal = as.detail(id);
+        if(deleteAnimal != null){
+            model.addAttribute("animal", deleteAnimal);
+            result = "animals/delete";
+        } else {
+            result = "redirect:/animales";
+        }
+        return result;
     }
+
+    @PostMapping("/animales/borrar/{id}")
+    public String eliminarAnimal(@PathVariable int id){
+        as.delete(id);
+        return "redirect:/animales";
+    }
+
 }
